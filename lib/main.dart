8c2d27e6235d5b62066260fa7999e11076cbfe2c
Ernesto2014 <jaimesalazar354@gmail.com>
2022-10-21 00:57:35 -0600

    Parcial
    api

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,154 @@
+import 'dart:convert';
+import 'package:parcial3/modelos/StarWarsObject.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(WebFlutter());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class WebFlutter extends StatefulWidget {
+  @override
+  State<WebFlutter> createState() => _WebFlutterState();
+}
 
-  // This widget is the root of your application.
+class _WebFlutterState extends State<WebFlutter> {
+  late Future<List<StarWarsObject>> _listadoStarWarsObject;
+
+  Future<List<StarWarsObject>> _getStarWarsObject() async {
+    final response = await http.get(Uri.parse("https://swapi.dev/api/people/"));
+    String cuerpo;
+    List<StarWarsObject> lista = [];
+
+    if (response.statusCode == 200) {
+      print(response.body);
+      cuerpo = utf8.decode(response.bodyBytes);
+      final jsonData = jsonDecode(cuerpo);
+      for (var item in jsonData["results"]) {
+        lista.add(StarWarsObject(item["name"], item["skin_color"], item["birth_year"], 
+                                item["url"], "https://starwars-visualguide.com/assets/img/characters/"+item["url"].replaceAll(new RegExp(r'[^0-9]'),'')+".jpg"));
+        
+      }
+    } else {
+      throw Exception("Falla en conexion  estado 500");
+    }
+    return lista;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _listadoStarWarsObject = _getStarWarsObject();
+  }
+
   @override
   Widget build(BuildContext context) {
+    var futureBuilder = FutureBuilder(
+      future: _listadoStarWarsObject,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView(
+            children: _listadoStarWarsObjects(snapshot.data),
+          );
+        } else if (snapshot.hasError) {
+          print(snapshot.error);
+          return Text("Error");
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Consumo Webservice',
+      home: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.auto_awesome_sharp)
+            ),
+            title: Text('API Personajes StartWars '),
+          ),
+          //body: //futureBuilder),
+          body: new Builder(
+            
+        
+          builder: (BuildContext context) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                //search box
+                Padding(
+
+                  padding: const EdgeInsets.only(left: 25, right: 25, top: 5, bottom: 5),
+                  child: TextField(                    
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true, 
+                      fillColor: Color.fromRGBO(239, 241, 231, 1),
+                      prefixIconColor:Colors.white,//Color.fromRGBO(239, 241, 231, 1) ,  
+                      hintText:'JAIME ERNESTO SALAZAR PEÑA 2511922017',
+                      prefixIcon: Icon(Icons.people)
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 8,
+                  child: futureBuilder,
+                ),
+                // Expanded(
+                //   flex: 2,
+                //   child: Text("hello"),
+                //   ),
+
+              ],
+            );
+        }
+      )
+    )
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+  List<Widget> _listadoStarWarsObjects(data) {
+    List<Widget> startwarsObject = [];
+    for (var itempk in data) {
+      startwarsObject.add(Card(
+        elevation: 2.0,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            // Text(itempk.num),
+            SizedBox(height:20),
+            Container(
+              padding: EdgeInsets.all(2.0),
+              height: 250,
+              width: 200,
+              
+               decoration: BoxDecoration(
+                
+                borderRadius: BorderRadius.circular(20),
+                 image: DecorationImage(
+                     image: NetworkImage(itempk.img),
+                     fit: BoxFit.cover),
+               ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            SizedBox(height:10),
+            Text("Nombre:" + itempk.name),
+            SizedBox(height:5),
+            Text("Color de piel: " + itempk.skin_color),
+            SizedBox(height:5),
+            Text("Año de nacimiento: " + itempk.birth_year),
+            SizedBox(height:15),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      ));
+    }
+    return startwarsObject;
   }
 }
